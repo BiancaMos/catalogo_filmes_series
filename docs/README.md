368107c40849165a2865e723c11f585ab9d6101a
Bianca Mós <78977958+BiancaMos@users.noreply.github.com>
2023-02-08 22:06:18 -0300

    Add files via upload

---
+++ docs/README.md
@@ -0,0 +1,30 @@
+# 🎞 Catálogo de filmes e séries
+### 🎥 Movie catalog
+<br>
+
+📸 Screenshot
+![](../images/tela1.png)
+
+#  🚀 Technologies
+
+- [HTML](##HTML)
+- [CSS](##CSS)
+- [JavaScript](##JavaScript)
+- [Github](##github)
+- [Codepen](##codepen)
+- [VScode](##vscode)
+
+# 👩🏻‍💻 Project
+Project made in immersion dev_ Alura's course of development for beginner
+
+🍿
+
+PT-BR: Projeto feito em curso imersão dev_ curso de desenvolvimento para iniciante da Alura
+
+
+ #
+ 
+                        Feito com 🤍 by BM
+
+
+ #### 🔎 **[Online project here](https://codepen.io/biancamos/full/RwKREYL)** Projeto desenvolvido em Agosto de 2022.
---
+++ script.js
@@ -0,0 +1,75 @@
+var listaFilmes = ["https://m.media-amazon.com/images/M/MV5BMTYzMDM4NzkxOV5BMl5BanBnXkFtZTgwNzM1Mzg2NzM@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BMDljNTQ5ODItZmQwMy00M2ExLTljOTQtZTVjNGE2NTg0NGIxXkEyXkFqcGdeQXVyODkzNTgxMDg@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BZjdkOTU3MDktN2IxOS00OGEyLWFmMjktY2FiMmZkNWIyODZiXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg","https://m.media-amazon.com/images/M/MV5BZDhlMzY0ZGItZTcyNS00ZTAxLWIyMmYtZGQ2ODg5OWZiYmJkXkEyXkFqcGdeQXVyODkzNTgxMDg@._V1_UX182_CR0,0,182,268_AL_.jpg", "https://m.media-amazon.com/images/M/MV5BYjJiZmE5ZDgtYWUxZi00MWI1LTg2MmEtZmUwZGE2YzRkNTE5XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_UX182_CR0,0,182,268_AL_.jpg"]
+
+
+for (var i = 0; i < listaFilmes.length; i++){
+  document.write("<img src=" + listaFilmes[i] + ">")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//var filmes = ["Star Wars", "Toy story","Interstrellar", "The Mandalorian", "WandaVision"]
+//var tentativas = 3 
+
+//while (tentativas > 0){
+  
+  //tentativas = tentativas - 1
+//}
+
+//for (var i = 0; i < filmes.length; i++){
+  //console.log(filmes[i])
+  
+//}
+
+// multiplicação sem multiplicar
+//var x = 2
+
+//x = x + x 
+
+//console.log(x)
+
+
+//i--
+//i++
+//console.log(filmes[0])
+//console.log(filmes[1])
+//console.log(filmes[2])
+
+//console.log(filmes.length)
+
+
+
+
+
+//var filme1 = "Star Wars"
+//var filme2 = "Toy Story"
+//var filme3 = "Interstrellar"
+
+//var filmes = ["Star Wars", "Toy Story", "Interstrellar"] //os colchetes servem para juntar as informações. Como se fosse uma coisa só. Apenas estamos declarando que existe no nosso código.
+
+//filmes.push("Star Wars") // o push sig. empurrar 
+//filmes.push("Toy Story")
+//filmes.push("Interstrellar")
+
+//console.log(filme1)
+//console.log(filme2)
+//console.log(filme3)
+
+//Array [] é para listas. Quando utilizamos Array's sempre o primeiro elemento será zero e náo 1. 
